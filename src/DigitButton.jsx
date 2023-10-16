4ce18a4c085e8270eeafe28136e73abd81816ff8
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-10-16 10:43:56 +0530

    Created a Basic Calculator

---
+++ src/DigitButton.jsx
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./App";
+
+function DigitButton({ digit, dispatch }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
+
+export default DigitButton;
